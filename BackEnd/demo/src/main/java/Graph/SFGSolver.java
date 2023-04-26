e2dbc70ac430745e18554d3080342143089477a4
ziadreda72 <engziadreda@gmail.com>
2023-04-26 15:29:07 +0200

    Forward Paths & Loops

--- BackEnd/demo/src/main/java/Graph/SFGSolver.java
+++ BackEnd/demo/src/main/java/Graph/SFGSolver.java
@@ -8,10 +8,28 @@ public class SFGSolver {
     private List<List<Integer>> forwardPaths = new ArrayList<>();
     private List<Integer> forwardPathsGains = new ArrayList<>();
     private List<List<Integer>> loops = new ArrayList<>();
+    private List < Integer > loopGains = new ArrayList<>();
 
     public SFGSolver(List < List < Pair > > adj){
         this.adj = adj;
-        computeAllForwardPaths();
+        PLAY();
+    }
+
+    private void PLAY(){
+        computeLoops();
+    }
+
+    private int getPathGain(List < Integer > path){
+        int g = 1;
+        for (int i = 0 ; i + 1 < path.size() ; ++i){
+            for (var pair : adj.get(path.get(i))){
+                if(((Integer) pair.F).equals(path.get(i + 1))){
+                    g *= pair.S;
+                    break;
+                }
+            }
+        }
+        return g;
     }
 
     private void computeAllForwardPaths(){
@@ -20,14 +38,16 @@ public class SFGSolver {
         currentPath.add(1);
         int n = (int) adj.size() - 1;
         forwardPathsDFS(1 , n , visited , currentPath);
+        for (var path : forwardPaths){
+            forwardPathsGains.add(getPathGain(path));
+        }
     }
 
     private void forwardPathsDFS(int src, int dist , boolean[] visited , List <Integer> currentPath){
         
         if (src == dist){
-            List<Integer> addlist =  new ArrayList<>();
-            addlist.addAll(currentPath);
-            forwardPaths.add(addlist);
+            List<Integer> addList = new ArrayList<>(currentPath);
+            forwardPaths.add(addList);
             return;
         }
 
@@ -40,6 +60,40 @@ public class SFGSolver {
             }
         }
         visited[src] = false;
+    }
+
+    private void computeLoops(){
+        for (int i = 1 ; i < adj.size() ; i++){
+            boolean[] visited = new boolean[adj.size()];
+            for (int j = 1 ; j < i ; j++) visited[j] = true;
+            List < Integer > loop = new ArrayList<>();
+            loopDFS(i , i , visited , loop);
+        }
+        for(var loop : loops){
+            loopGains.add(getPathGain(loop));
+        }
+//        for (int i = 0 ; i < loops.size() ; i++){
+//            System.out.print("Loop #" + (i + 1) + ": ");
+//            for (var x : loops.get(i)){
+//                System.out.print(x + " ");
+//            }
+//            System.out.println("\nGain: " + loopGains.get(i));
+//        }
+    }
+
+    private void loopDFS(int src , int start , boolean[] visited , List < Integer > loop){
+        visited[src] = true;
+        loop.add(src);
+        for (var pair : adj.get(src)){
+            if (!visited[pair.F]){
+                loopDFS(pair.F , start , visited , loop);
+            }else if(visited[pair.F] && pair.F == start){
+                List < Integer > addList = new ArrayList<>(loop);
+                addList.add(start);
+                loops.add(addList);
+            }
+        }
+        loop.remove(loop.size() - 1);
     }
 
     public List<List<Integer>> getForwardPaths() {
@@ -64,5 +118,13 @@ public class SFGSolver {
 
     public void setLoops(List<List<Integer>> loops) {
         this.loops = loops;
+    }
+
+    public List<Integer> getLoopGains() {
+        return loopGains;
+    }
+
+    public void setLoopGains(List<Integer> loopGains) {
+        this.loopGains = loopGains;
     }
 }
--- BackEnd/demo/src/main/java/com/example/demo/BackEndApplication.java
+++ BackEnd/demo/src/main/java/com/example/demo/BackEndApplication.java
@@ -15,24 +15,29 @@ public class BackEndApplication {
 		SpringApplication.run(BackEndApplication.class, args);
 
 		List <List<Pair>> adj = new ArrayList<>();
-		for (int i = 0 ; i < 6 ; i++) adj.add(new ArrayList<>());
+		for (int i = 0 ; i < 7 ; i++) adj.add(new ArrayList<>());
 		adj.get(1).add(new Pair(2 , 1));
-		adj.get(2).add(new Pair(3 , 1));
-		adj.get(3).add(new Pair(4 , 1));
-		adj.get(4).add(new Pair(5 , 1));
-		adj.get(2).add(new Pair(4 , 1));
-		adj.get(3).add(new Pair(5 , 1));
+		adj.get(2).add(new Pair(3 , 2));
+		adj.get(3).add(new Pair(4 , 3));
+		adj.get(4).add(new Pair(5 , 4));
+		adj.get(5).add(new Pair(6 , 5));
+		adj.get(4).add(new Pair(1 , 6));
+		adj.get(5).add(new Pair(1 , 7));
+		adj.get(6).add(new Pair(2 , 8));
 
-		List<List<Integer>> paths = new SFGSolver(adj).getForwardPaths();
+		SFGSolver solver = new SFGSolver(adj);
 
-		for(int i=0;i<paths.size();i++){
-			System.out.print("Path "+i+" :");
-			for(int j=0;j<paths.get(i).size();j++){
-				System.out.print(paths.get(i).get(j)+" ");
-			}
-
-			System.out.println();
-		}
+//		var paths = solver.getForwardPaths();
+//		var gains = solver.getForwardPathsGains();
+//
+//		for(int i=0;i<paths.size();i++){
+//			System.out.print("Path "+ i +": ");
+//			for(int j=0;j<paths.get(i).size();j++){
+//				System.out.print(paths.get(i).get(j)+" ");
+//			}
+//			System.out.println();
+//			System.out.println("Gain: " + gains.get(i));
+//		}
 	}
 
 }
