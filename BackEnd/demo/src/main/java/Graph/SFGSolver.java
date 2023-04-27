4b31d936cd53cc52d7821f593d201258ec37f378
ziadreda72 <engziadreda@gmail.com>
2023-04-28 01:17:17 +0300

    Back is Done 3aaaaa :'D

--- BackEnd/demo/src/main/java/Graph/SFGSolver.java
+++ BackEnd/demo/src/main/java/Graph/SFGSolver.java
@@ -1,14 +1,20 @@
 package Graph;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class SFGSolver {
-    private List<List<Pair>> adj = new ArrayList<>();
-    private List<List<Integer>> forwardPaths = new ArrayList<>();
-    private List<Integer> forwardPathsGains = new ArrayList<>();
-    private List<List<Integer>> loops = new ArrayList<>();
+    private final List <List < Pair > > adj;
+    private List< List < Integer > > forwardPaths = new ArrayList<>();
+    private List< Integer > forwardPathsGains = new ArrayList<>();
+    private List< List < Integer > > loops = new ArrayList<>();
     private List < Integer > loopGains = new ArrayList<>();
+    private final List < Integer > delta = new ArrayList<>();
+    private double numerator;
+    private double Delta;
+    private double result;
+
 
     public SFGSolver(List < List < Pair > > adj){
         this.adj = adj;
@@ -16,7 +22,28 @@ public class SFGSolver {
     }
 
     private void PLAY(){
+        computeAllForwardPaths();
         computeLoops();
+        Delta = getDelta(loops);
+        computeDeltaArray();
+        result = computeResult();
+    }
+
+    private boolean checkNonTouching(List < List < Integer > > paths){
+        HashMap < Integer , Integer > map = new HashMap<>();
+        for(var path : paths){
+            for (var i : path){
+                if(map.containsKey(i)){
+                    map.put(i , map.get(i) + 1);
+                }else{
+                    map.put(i , 1);
+                }
+            }
+        }
+        for (var keyValue : map.entrySet()){
+            if(keyValue.getValue() > 1) return false;
+        }
+        return true;
     }
 
     private int getPathGain(List < Integer > path){
@@ -36,21 +63,19 @@ public class SFGSolver {
         boolean[] visited = new boolean[adj.size()];
         List<Integer> currentPath = new ArrayList<>();
         currentPath.add(1);
-        int n = (int) adj.size() - 1;
+        int n = adj.size() - 1;
         forwardPathsDFS(1 , n , visited , currentPath);
         for (var path : forwardPaths){
             forwardPathsGains.add(getPathGain(path));
         }
     }
 
-    private void forwardPathsDFS(int src, int dist , boolean[] visited , List <Integer> currentPath){
-        
+    private void forwardPathsDFS(int src, int dist , boolean[] visited , List < Integer > currentPath){
         if (src == dist){
             List<Integer> addList = new ArrayList<>(currentPath);
             forwardPaths.add(addList);
             return;
         }
-
         visited[src] = true;
         for (Pair pair : adj.get(src)){
             if(!visited[pair.F]){
@@ -72,13 +97,6 @@ public class SFGSolver {
         for(var loop : loops){
             loopGains.add(getPathGain(loop));
         }
-//        for (int i = 0 ; i < loops.size() ; i++){
-//            System.out.print("Loop #" + (i + 1) + ": ");
-//            for (var x : loops.get(i)){
-//                System.out.print(x + " ");
-//            }
-//            System.out.println("\nGain: " + loopGains.get(i));
-//        }
     }
 
     private void loopDFS(int src , int start , boolean[] visited , List < Integer > loop){
@@ -96,27 +114,71 @@ public class SFGSolver {
         loop.remove(loop.size() - 1);
     }
 
-    public List<List<Integer>> getForwardPaths() {
+    private int getDelta(List < List < Integer > > loops){
+        int n = loops.size();
+        int ret = 1;
+        for (int msk = 1 ; msk < (1 << n) ; msk++){
+            List < List < Integer > > sub = new ArrayList<>() , sub2 = new ArrayList<>();
+            for (int j = 0 ; j < n ; j++){
+                if(((1 << j) & msk) >= 1){
+                    List < Integer > loop = new ArrayList<>();
+                    sub.add(loop);
+                    sub2.add(loops.get(j));
+                }
+            }
+            boolean check = checkNonTouching(sub);
+            if (!check) continue;
+            int g = 1;
+            for (var path : sub2) g *= getPathGain(path);
+            if ((Integer.bitCount(msk) & 1) == 1) ret -= g;
+            else ret += g;
+        }
+        return ret;
+    }
+
+    private void computeDeltaArray(){
+        for (var path : forwardPaths){
+            List < List < Integer > > nonTouching = new ArrayList<>();
+            for (var loop : loops){
+                List < List < Integer > > list = new ArrayList<>();
+                list.add(path);
+                list.add(loop);
+                if(checkNonTouching(list)) nonTouching.add(loop);
+            }
+            delta.add(getDelta(nonTouching));
+        }
+    }
+
+    private double computeResult(){
+        double x = 0;
+        for (int i = 0 ; i < forwardPathsGains.size() ; i++){
+            x += (double) forwardPathsGains.get(i) * delta.get(i);
+        }
+        setNumerator(x);
+        return x / Delta;
+    }
+
+    public List < List < Integer > > getForwardPaths() {
         return forwardPaths;
     }
 
-    public void setForwardPaths(List<List<Integer>> forwardPaths) {
+    public void setForwardPaths(List < List < Integer > > forwardPaths) {
         this.forwardPaths = forwardPaths;
     }
     
-    public List<Integer> getForwardPathsGains() {
+    public List < Integer > getForwardPathsGains() {
         return forwardPathsGains;
     }
 
-    public void setForwardPathsGains(List<Integer> forwardPathsGains) {
+    public void setForwardPathsGains(List < Integer > forwardPathsGains) {
         this.forwardPathsGains = forwardPathsGains;
     }
 
-    public List<List<Integer>> getLoops() {
+    public List < List < Integer > > getLoops() {
         return loops;
     }
 
-    public void setLoops(List<List<Integer>> loops) {
+    public void setLoops(List < List < Integer > > loops) {
         this.loops = loops;
     }
 
@@ -126,5 +188,29 @@ public class SFGSolver {
 
     public void setLoopGains(List<Integer> loopGains) {
         this.loopGains = loopGains;
+    }
+
+    public double getDelta() {
+        return Delta;
+    }
+
+    public void setDelta(double delta) {
+        Delta = delta;
+    }
+
+    public double getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(double numerator) {
+        this.numerator = numerator;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
     }
 }
--- BackEnd/demo/src/main/java/com/example/demo/BackEndApplication.java
+++ BackEnd/demo/src/main/java/com/example/demo/BackEndApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @SpringBootApplication
@@ -21,15 +22,29 @@ public class BackEndApplication {
 		adj.get(3).add(new Pair(4 , 3));
 		adj.get(4).add(new Pair(5 , 4));
 		adj.get(5).add(new Pair(6 , 5));
-		adj.get(4).add(new Pair(1 , 6));
-		adj.get(5).add(new Pair(1 , 7));
-		adj.get(6).add(new Pair(2 , 8));
+		adj.get(2).add(new Pair(1 , 2));
+		adj.get(4).add(new Pair(3 , 2));
+		adj.get(6).add(new Pair(5 , 2));
 
 		SFGSolver solver = new SFGSolver(adj);
+		System.out.println(solver.getNumerator() + " / " + solver.getDelta());
+//		System.out.println(solver.getDelta());
+		var paths = solver.getLoops();
+		var gains = solver.getLoopGains();
 
-//		var paths = solver.getForwardPaths();
-//		var gains = solver.getForwardPathsGains();
+//		HashMap< Integer , Integer > map = new HashMap<>();
+//		for (var i : paths.get(0)){
+//			if(map.containsKey(i)){
+//				map.put(i , map.get(i) + 1);
+//			}else{
+//				map.put(i , 1);
+//			}
+//		}
 //
+//		for (var keyValue : map.entrySet()){
+//			System.out.println(keyValue.getKey() + " " + keyValue.getValue());
+//		}
+
 //		for(int i=0;i<paths.size();i++){
 //			System.out.print("Path "+ i +": ");
 //			for(int j=0;j<paths.get(i).size();j++){
